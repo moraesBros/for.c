f67e8d73d71fc5d5594215059b975933a6a1f649
moraesBros <161986858+moraesBros@users.noreply.github.com>
2025-05-02 16:11:56 -0300

    Create for_traco.c

---
+++ for_traco.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+int linha, coluna;
+    {for( linha = 1; linha < 24; linha ++)
+        for( coluna = 1; coluna < 40; coluna ++)
+    printf("-");
+    putchar('\n');
+    }
+    return 0;
+}
